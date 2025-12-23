8236114fe698aff7562fffdd563673f31d4e2c50
Vitor Jau <vitor.jau@dtidigital.com.br>
2025-12-23 00:17:33 -0300

    add compiler dist

--- Back-end/dist/main.js
+++ Back-end/dist/main.js
@@ -5,12 +5,17 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const corsOrigin = process.env.CORS_ORIGIN
+        ? process.env.CORS_ORIGIN.split(',').map(o => o.trim())
+        : '*';
     app.enableCors({
-        origin: '*',
+        origin: corsOrigin,
         credentials: false,
     });
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, transform: true }));
-    await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
+    const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+    await app.listen(port, '0.0.0.0');
+    console.log(`Backend running on port ${port}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
